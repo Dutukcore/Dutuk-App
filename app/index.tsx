fe0dabed9bec82cdd2160434ed66cb173ba53d7d
DharshanPalani <dharshanpalani.2006@gmail.com>
2025-06-10 23:02:54 +0530

    Styled the input section For no reason I made it so that it checks if there is session if so direct to home or auth page

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,32 @@
+import { Redirect } from "expo-router";
+import { useEffect, useState } from "react";
 import { View } from "react-native";
+import { supabase } from "../utils/supabase";
 import UserAuth from "./auth/UserAuth";
+
 export default function Index() {
-  return (
+  const [loading, setLoading] = useState(true);
+  const [isSignedIn, setIsSignedIn] = useState(false);
+
+  // This part checks if there is any session stored if yes router to home or auth page
+  useEffect(() => {
+    const check = async () => {
+      const { data } = await supabase.auth.getSession();
+      if (data.session) {
+        setIsSignedIn(true);
+      } else {
+        setIsSignedIn(false);
+      }
+      setLoading(false);
+    };
+    check();
+  }, []);
+
+  if (loading) return null;
+
+  return isSignedIn ? (
+    <Redirect href="/(tabs)/home" />
+  ) : (
     <View
       style={{
         flex: 1,
--- app/profile/companyBasicInfo/companyInfoPage.tsx
+++ app/profile/companyBasicInfo/companyInfoPage.tsx
@@ -1,16 +1,34 @@
 import React from "react";
-import { TextInput, View } from "react-native";
+import { StyleSheet, TextInput, View } from "react-native";
 
 const companyInfoPage = () => {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <TextInput placeholder="Company name" editable={false} />
-      <TextInput placeholder="Mail" editable={false} />
-      <TextInput placeholder="Address" editable={false} />
-      <TextInput placeholder="Phone number" editable={false} />
-      <TextInput placeholder="Website" editable={false} />
+      <TextInput
+        style={style.input}
+        placeholder="Company name"
+        editable={false}
+      />
+      <TextInput style={style.input} placeholder="Mail" editable={false} />
+      <TextInput style={style.input} placeholder="Address" editable={false} />
+      <TextInput
+        style={style.input}
+        placeholder="Phone number"
+        editable={false}
+      />
+      <TextInput style={style.input} placeholder="Website" editable={false} />
     </View>
   );
 };
 
 export default companyInfoPage;
+
+const style = StyleSheet.create({
+  input: {
+    borderWidth: 1,
+    borderColor: "black",
+    padding: 10,
+    margin: 15,
+    width: 250,
+  },
+});
