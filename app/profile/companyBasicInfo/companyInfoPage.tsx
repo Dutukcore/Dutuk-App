ced88c8babdb74ce84c42644be9dbc3b5b3f9a43
DharshanPalani <dharshanpalani.2006@gmail.com>
2025-06-11 11:44:14 +0530

    Modularized Company info section

--- app/profile/companyBasicInfo/companyInfoPage.tsx
+++ app/profile/companyBasicInfo/companyInfoPage.tsx
@@ -1,11 +1,6 @@
+import EditableInputField from "@/components/EditableInputField";
 import React, { useState } from "react";
-import {
-  StyleSheet,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  View,
-} from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 const CompanyInfoPage = () => {
   const [company, setCompany] = useState(false);
@@ -17,76 +12,31 @@ const CompanyInfoPage = () => {
   return (
     <View style={styles.screen}>
       <Text style={styles.headerStyle}>Company Basic Info</Text>
-      <View style={styles.inputRow}>
-        <TextInput
-          style={[styles.input, company && styles.inputEditable]}
-          placeholder="Company name"
-          editable={company}
-        />
-        <TouchableOpacity
-          style={styles.editButton}
-          onPress={() => setCompany(!company)}
-        >
-          {/* A edit emoji */}
-          <Text style={styles.editText}>📝</Text>
-        </TouchableOpacity>
-      </View>
-      <View style={styles.inputRow}>
-        <TextInput
-          style={[styles.input, mail && styles.inputEditable]}
-          placeholder="Mail"
-          editable={mail}
-        />
-        <TouchableOpacity
-          style={styles.editButton}
-          onPress={() => setMail(!mail)}
-        >
-          {/* A edit emoji */}
-          <Text style={styles.editText}>📝</Text>
-        </TouchableOpacity>
-      </View>
-      <View style={styles.inputRow}>
-        <TextInput
-          style={[styles.input, address && styles.inputEditable]}
-          placeholder="Address"
-          editable={address}
-        />
-        <TouchableOpacity
-          style={styles.editButton}
-          onPress={() => setAddress(!address)}
-        >
-          {/* A edit emoji */}
-          <Text style={styles.editText}>📝</Text>
-        </TouchableOpacity>
-      </View>
-      <View style={styles.inputRow}>
-        <TextInput
-          style={[styles.input, phone && styles.inputEditable]}
-          placeholder="Phone Number"
-          editable={phone}
-        />
-        <TouchableOpacity
-          style={styles.editButton}
-          onPress={() => setPhone(!phone)}
-        >
-          {/* A edit emoji */}
-          <Text style={styles.editText}>📝</Text>
-        </TouchableOpacity>
-      </View>
-      <View style={styles.inputRow}>
-        <TextInput
-          style={[styles.input, website && styles.inputEditable]}
-          placeholder="Website"
-          editable={website}
-        />
-        <TouchableOpacity
-          style={styles.editButton}
-          onPress={() => setWebsite(!website)}
-        >
-          {/* A edit emoji */}
-          <Text style={styles.editText}>📝</Text>
-        </TouchableOpacity>
-      </View>
+      <EditableInputField
+        editable={company}
+        onToggleEdit={() => setCompany(!company)}
+        placeholder="Company name"
+      />
+      <EditableInputField
+        editable={mail}
+        onToggleEdit={() => setMail(!mail)}
+        placeholder="Mail"
+      />
+      <EditableInputField
+        editable={phone}
+        onToggleEdit={() => setPhone(!phone)}
+        placeholder="Phone number"
+      />
+      <EditableInputField
+        editable={address}
+        onToggleEdit={() => setAddress(!address)}
+        placeholder="Address"
+      />
+      <EditableInputField
+        editable={website}
+        onToggleEdit={() => setWebsite(!website)}
+        placeholder="Website"
+      />
     </View>
   );
 };
@@ -99,33 +49,6 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     paddingHorizontal: 20,
     backgroundColor: "#f5f5f5",
-  },
-  inputRow: {
-    flexDirection: "row",
-    alignItems: "center",
-    marginBottom: 15,
-  },
-  input: {
-    flex: 1,
-    borderWidth: 1,
-    borderColor: "#ccc",
-    padding: 12,
-    borderRadius: 8,
-    backgroundColor: "#e9e9e9",
-  },
-  inputEditable: {
-    backgroundColor: "#ffffff",
-    borderColor: "#007BFF",
-  },
-  editButton: {
-    marginLeft: 10,
-    padding: 10,
-    backgroundColor: "black",
-    borderRadius: 8,
-  },
-  editText: {
-    color: "white",
-    fontSize: 16,
   },
   headerStyle: {
     fontSize: 30,
