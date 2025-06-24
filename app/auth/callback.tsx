1ba92efa440db740991eae231c5d7a7806a38e29
Dharshan Palani <dharshanpalani.2006@gmail.com>
2025-06-24 11:10:38 +0530

    implimented func on callback to check if the user data is present in the userByRole if so skip if not then insert new and then route to the home page

--- app/auth/callback.tsx
+++ app/auth/callback.tsx
@@ -7,19 +7,46 @@ export default function AuthCallback() {
   const router = useRouter();
 
   useEffect(() => {
-  const handleDeepLink = async () => {
-    const url = await Linking.getInitialURL();
+    const handleDeepLink = async () => {
+      const url = await Linking.getInitialURL();
 
-    if (url?.includes("code=")) {
-      const { data, error } = await supabase.auth.exchangeCodeForSession(url);
-      if (error) console.error("exchangeCodeForSession error", error);
-      else router.replace("/(tabs)/home");
-    }
-  };
+      if (url?.includes("code=")) {
+        const { data, error } = await supabase.auth.exchangeCodeForSession(url);
+        if (error) console.error("exchangeCodeForSession error", error);
 
-  handleDeepLink();
-}, []);
+        const user = data?.user;
 
+        if (user) {
+          const { data: existingUser, error: fetchError } = await supabase
+            .from("userByRole")
+            .select("id")
+            .eq("id", user.id)
+            .single();
+          if (fetchError) {
+            console.log("Error when fetching" + fetchError);
+            return;
+          }
+          if (!existingUser) {
+            const { error: insertError } = await supabase
+              .from("userByRole")
+              .insert({
+                id: user.id,
+                email: user.email,
+                role: "vendor",
+                created_at: new Date().toISOString(),
+              });
+            if (insertError) {
+              console.log("Error when inserting " + insertError);
+              return;
+            }
+          }
+          router.replace("/(tabs)/home");
+        }
+      }
+    };
+
+    handleDeepLink();
+  }, []);
 
   return null;
 }
