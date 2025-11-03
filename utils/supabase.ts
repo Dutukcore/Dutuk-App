d32f36c9e7698a236ddc4d21027447813f7d6d32
CatifiedSWE <smnaveen124@gmail.com>
2025-11-03 19:31:48 +0530

    Update

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -4,6 +4,8 @@ const supabaseUrl = "https://unqpmwlzyaqrryzyrslf.supabase.co";
 const supabaseAnonKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVucXBtd2x6eWFxcnJ5enlyc2xmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg2MTI5ODUsImV4cCI6MjA2NDE4ODk4NX0.RwDqMBy9ctQzxG9CuVzaUlriK6M5cGycPhQviNQfcxw";
 
+
+  
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
     autoRefreshToken: true,
